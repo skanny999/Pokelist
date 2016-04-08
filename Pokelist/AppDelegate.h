de7133cd177240052475947019b0bf1aa70dcb3d
skanny999 <skanny999@me.com>
2016-04-08 13:47:22 +0100

    Initial Commit

---
+++ Pokelist/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pokelist
+//
+//  Created by Riccardo Scanavacca on 08/04/2016.
+//  Copyright © 2016 Riccardo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
